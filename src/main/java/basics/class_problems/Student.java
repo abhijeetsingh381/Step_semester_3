4c906ad113fa426345b83971380d64bd71b3b8ea
Abhijeet Singh <abhijeet.tanwar01@gmail.com>
2026-09-13 15:29:06 +0530

    Add Student Record Class

---
+++ src/main/java/basics/class_problems/Student.java
@@ -0,0 +1,17 @@
+package main.java.basics.class_problems;
+
+public class Student {
+
+    String name;
+    int marks;
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+        student.name = "Divya";
+        student.marks = 88;
+
+        System.out.println("Name: " + student.name + " | Marks: " + student.marks);
+    }
+}
